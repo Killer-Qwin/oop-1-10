e0caff685777a2f82942683b306940021349fcb6
Mingazizov Nicita <113692942+Killer-Qwin@users.noreply.github.com>
2023-04-04 21:47:43 +0300

    Add files via upload

---
+++ oop.js
@@ -0,0 +1,116 @@
+// class Employee { 
+//     name; 
+//     age; 
+//     salary; 
+//     orientation(){ 
+//         return `gay`
+//     } 
+// } 
+//     let maxs = new Employee 
+//   maxs.name = `max `
+//   maxs.age = `12litrov`
+//   maxs.salary = `50 ` 
+ 
+//     let nicita = new Employee 
+//     nicita.name = `nicita`
+//     nicita.age = `17litrov `
+//     nicita.salary = `60` 
+ 
+//     let Dida_Bogdan = new Employee 
+//     Dida_Bogdan.name = `Dida_Bogdan` 
+//     Dida_Bogdan.age = `122litrov `
+//     Dida_Bogdan.salary = `50 `
+ 
+// document.write( maxs.name, " ", maxs.age, " ", maxs.salary) 
+// let sum =  +Dida_Bogdan.salary +  +nicita.salary +  +maxs.salary + `mg Brusko `
+// console.log(sum) 
+// console.log(maxs.orientation())
+
+// class Employee{
+//     show(name, salary){
+//         return name + ` ` + salary
+//     }
+// }
+// let user = new Employee;
+// console.log(user.show(`maxs`, `150 tugrikof`))
+
+// class Employee{
+//     show(){
+//         console.log(this.name)
+//         console.log(this.salari)
+//     }
+// }
+// let user = new Employee;
+// user.name = `max`;
+// user.salari = `150 tugrikof`;
+// user.show()
+
+// class User {
+// 	show() {
+// 		return this.cape(this.name);
+// 	}	
+// 	cape(str) {
+// 		return str[0].toUpperCase() + str.slice(1);
+// 	}
+// }
+// let user = new User
+// user.name = `max`
+// console.log(user.show())
+
+// class Student{
+//     names(){
+//         return this.cape(this.name);
+//     }
+//     surns(){
+//         return this.cape(this.surn);
+//     }
+//     cape(str) {
+// 		return str[0].toUpperCase() + str.slice(1);
+// 	}
+//     ins()
+// }
+// let user = new Student;
+// user.name = `max`;
+// user.surn = `daynidov`;
+// console.log(user.names())
+// console.log(user.surns())
+
+// class Student{
+//     names(){
+//         return this.cape(this.name);
+//     }
+//     surns(){
+//         return this.cape(this.surn);
+//     }
+//     cape(str) {
+// 		return str[0].toUpperCase();
+// 	}
+// }
+// let user = new Student;
+// user.name = `max`;
+// user.surn = `daynidov`;
+// console.log(user.names() + user.surns())
+
+// class Student{
+//     name = 'max';
+//     surn = `daym`;
+//     names(){
+//         return this.cape(this.name);
+//     }
+//     surns(){
+//         return this.cape(this.surn);
+//     }
+//     cape(str) {
+// 		return str[0].toUpperCase() + str.slice(1);
+// 	}
+// }
+// let user = new Student;
+// console.log(user.names())
+// console.log(user.surns())
+
+// class Employee{
+//     constructor(){
+//         console.log(`Вася Пупкин`)
+//     }
+// }
+// new Employee
